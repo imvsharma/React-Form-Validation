3cba603a0a9dd663bb92c352a9d785db8b46246e
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2019-04-17 07:57:43 +0530

    create companycomponent

---
+++ src/pages/companyRegistration/companyRegistration.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './companyRegistration.css';
+
+export default class CompanyRegistration extends React.Component {
+    render () {
+        return (
+            <div className="form-heading">General Information</div>
+        )
+    }
+}
